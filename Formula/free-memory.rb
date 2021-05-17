2c2feb5234f83fd3bf4f416c677a578be4184494
jxweng <jx.w@me.com>
2021-05-17 09:18:54 +0900

    add free memory

---
+++ Formula/free-memory.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Archey < Formula
+  desc ""
+  homepage "https://github.com/jxweng/Utilities"
+  url "https://raw.githubusercontent.com/jxweng/Utilities/main/cli/free-memory"
+  version "27602"
+  sha256 "18a712d29a388eba6669db7a1fd47ef521ff73281c9685b87188deef6c6e8033"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    bin.install 'free-memory'
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test imgcat`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
