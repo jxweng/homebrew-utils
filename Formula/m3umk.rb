ad7a4fb2e2e21c8087523834cf7abe9304e3691e
jxweng <jx.w@me.com>
2021-05-30 15:57:54 +0900

    add m3umk

---
+++ Formula/m3umk.rb
@@ -0,0 +1,32 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class M3UMK < Formula
+  desc ""
+  homepage "https://github.com/jxweng/Utilities"
+  url "https://raw.githubusercontent.com/jxweng/Utilities/main/cli/m3umk"
+  version "27602"
+  sha256 "c72b7b031677d44d03fa391b6130f985b336859d7be84f1781c32e160d69d87c"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    bin.install 'm3umk'
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test imgcat`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
